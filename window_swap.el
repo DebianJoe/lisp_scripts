5bf7a4cd7603bbe7fb51405b07318ef10a651e96
DebianJoe <jbpaladin27@gmail.com>
2014-11-01 09:29:30 -0500

    fixed dangerous iterator

--- window_swap.el
+++ window_swap.el
@@ -5,12 +5,12 @@
     ((not (> (count-windows) 1))
      (message "You can't rotate a single window!"))
     (t
-     (setq i 1)
+     (setq iterate 1)
      (setq numWindows (count-windows))
-     (while  (< i numWindows)
+     (while  (< iterate numWindows)
        (let* (
-              (w1 (elt (window-list) i))
-              (w2 (elt (window-list) (+ (% i numWindows) 1)))
+              (w1 (elt (window-list) iterate))
+              (w2 (elt (window-list) (+ (% iterate numWindows) 1)))
               (b1 (window-buffer w1))
               (b2 (window-buffer w2))
               (s1 (window-start w1))
@@ -19,6 +19,6 @@
          (set-window-buffer w2 b1)
          (set-window-start w1 s2)
          (set-window-start w2 s1)
-         (setq i (1+ i)))))))
+         (setq iterate (1+ iterate)))))))
 
 (global-set-key (kbd "C-c s") 'window_swap)
