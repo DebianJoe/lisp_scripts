8ee450e86b86527199ebd67ad97642c901f1fc74
DebianJoe <jbpaladin27@gmail.com>
2014-11-02 12:55:09 -0600

    eplayer updated

--- eplayer.el
+++ eplayer.el
@@ -3,11 +3,25 @@
 ;; Final call should be:
 ;; (start-process "cliquehop" "cliquehop" "mpg123" "http://xstream1.somafm.com:8062")
 
+;; Have these at top in case they need editing. ;;
+(setq chop '"http://xstream1.somafm.com:8062") ;; Cliqhop
+(setq dubs '"http://uwstream2.somafm.com:8406") ;; dubstep beyond
 
-;; TODO: Currently auto-plays, needs to prompt user ;;
-(defun eplayer (station) ; interactive version
-  "start a radio stream in new buffer"
-  (message "1 = cliquehop")
+;; set each station as a function to allow auto-complete fills ;;
+(defun eplayer-cliqhop (station)
+  "start a cliqhop radio stream in new buffer"
+  (message "Soma FM's Cliqhop")
   (interactive "p")
-  (if (equal station 1)
-      (start-process "cliquehop" "cliquehop" "mpg123" "http://xstream1.somafm.com:8062")(nil)))
+      (start-process "cliqhop"
+		     "cliqhop"
+		     "mpg123"
+		     "%s" chop))
+
+(defun eplayer-dubstep (station)
+  "start a radio stream in new buffer"
+  (message "Soma FM's Dubstep Beyond")
+  (interactive "p")
+      (start-process "dubstep"
+		     "dubstep_beyond"
+		     "mpg123"
+		     "%s" dubs))
