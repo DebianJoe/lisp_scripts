9a83438d99170e8060a1779d92a91fb23ab137c3
DebianJoe <jbpaladin27@gmail.com>
2014-11-04 00:47:57 -0600

    sonic universe to pls

--- eplayer.el
+++ eplayer.el
@@ -10,7 +10,7 @@
 (setq dubs '"http://uwstream2.somafm.com:8406")   ;; Dubstep Beyond
 (setq 7iso '"http://uwstream2.somafm.com:7770")   ;; Seven Inch Soul
 (setq doom '"http://xstream1.somafm.com:8300")    ;; Doomed
-(setq suni '"http://uwstream2.somafm.com:8600")   ;; Sonic Universe
+(setq suni '"http://somafm.com/sonicuniverse.pls");; Sonic Universe
 (setq agnt '"http://xstream1.somafm.com:8002")    ;; Secret Agent
 (setq ipop '"http://uwstream3.somafm.com:8090")   ;; Indy Pop Rocks
 (setq ffor '"http://xstream1.somafm.com:7400")    ;; Folk Forward
@@ -139,7 +139,7 @@
       (start-process "Sonic_Universe"
 		     "Sonic_Universe"
 		     "mpg123"
-		     "%s" suni))
+		     (concat "-@" suni)))
 
 (defun eplayer-secretagent (station)
   "start a Soma FM Secret Agent radio stream in new buffer"
