fb63726fc7eaf6d14826007633aa52d5bfb924a1
DebianJoe <jbpaladin27@gmail.com>
2014-11-06 23:47:00 -0600

    cool tricks

--- eshell_extension.el
+++ eshell_extension.el
@@ -1,5 +1,22 @@
+;; Allow clear in eshell ;;
 (defun eshell/clear ()
   "clears the eshell buffer"
   (interactive)
   (let ((inhibit-read-only t))
     (erase-buffer)))
+
+;; This will transform ansi color to faces in Emacs shell! ;;
+(ansi-color-for-comint-mode-on)
+(defun eshell-handle-ansi-color ()
+  (ansi-color-apply-on-region eshell-last-output-start
+			      eshell-last-output-end))
+(add-hook 'eshell-mode-hook
+	  '(lambda ()
+	     (add-to-list
+	      'eshell-output-filter-functions
+	      'eshell-handle-ansi-color)))
+
+;; Alias emacs to use find-file ;;
+(defun eshell/emacs (file)
+  "find-file alias"
+  (find-file file))
